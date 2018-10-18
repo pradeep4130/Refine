cde5c64efbc0b3ef904753d696ca66fc16e1b454
pradeep4130 <pradeeprai4130@gmail.com>
2018-10-18 01:57:45 -0500

    1: Clean up brand names

--- Refine.R
+++ Refine.R
@@ -1,5 +1,5 @@
 # Read Packages installed
-library()
+#library()
 
 # Load packages 
 library(gdata)
@@ -16,10 +16,21 @@ dim(refine)
 
 data.frame(refine)
 
-#Clean up company column 
+#Clean up company column for philips
 
-gsub("lips$","philips",refine$company)
+refine$company <- gsub(pattern = ".*lips$", replacement = "philips",refine$company)
 
-grepl("lips$",refine$company)
+refine$company <- gsub(pattern = "^phi.*", replacement = "philips", refine$company)
 
-refine %>% mutate(company = ifelse (grepl("lips$",company),"philips",company))
+#Clean up company column for akzo
+
+refine$company <- gsub(pattern = "^ak.*", replacement = "akzo", refine$company, ignore.case = TRUE)
+
+#Clean up company column for van houten
+
+refine$company <- gsub(pattern = "^van.*", replacement = "van houten", refine$company, ignore.case = TRUE)
+
+#Clean up company column for unilever
+
+refine$company <- gsub(pattern = "^uni.*", replacement = "unilever", refine$company, ignore.case = TRUE)
+View(refine)
